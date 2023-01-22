0bc58a48a882d426f7857a81a87a5d96e7ad15dd
mdparvej <m.parvej68@gmail.com>
2023-01-23 03:06:09 +0600

    new ama

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import react, {useState, useEffect} from "react"
 function App() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=500').then(res => res.json())
+    .then(data => setUser(data.results)).catch((error) => "error")
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{display:'grid' , gridTemplateColumns: 'auto auto auto auto auto auto auto auto auto auto auto'}}>
+        {
+          user.map(person => <img src={person.picture.large} alt="" srcset="" style={{width: '100px',border: '1px solid green'}}/>)
+          
+        }
       </header>
     </div>
   );
